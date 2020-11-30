7ea30e2f0cd329cff379e8900a202030b5cf3fd0
Harjinder <singh.harjinder.1057@gmail.com>
2020-11-30 20:42:10 +0530

    Landing Page UI Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ListCardComponent } from './list-card/list-card.component';
+import { ListViewerComponent } from './list-viewer/list-viewer.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListCardComponent,
+    ListViewerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterContentInit {
+  gridByBreakpoint = {
+    xl: 6,
+    lg: 4,
+    md: 3,
+    sm: 2,
+    xs: 1,
+  };
+  cols: number;
+  constructor(private mediaObserver: MediaObserver) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterContentInit() {
+    this.mediaObserver.asObservable().subscribe((change: MediaChange[]) => {
+      this.cols = this.gridByBreakpoint[change[0].mqAlias];
+    });
   }
-
 }
